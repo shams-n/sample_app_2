f572f421b65077437c97ad5c2667563fdfd7f9a2
Shams <shams.elnawawi@gmail.com>
2017-05-22 14:54:32 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
-
-<<<<<<< HEAD
-  #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> e48ce4e7693ce826052c71faf0f51c8806acda6a
+  
   root 'application#hello'
 end
